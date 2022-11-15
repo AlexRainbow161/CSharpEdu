feaea1a6a63d1a6e62964ed7dc7f41ce7aacba59
AlexRainbow161 <xander.rei@gmail.com>
2022-11-15 19:52:39 +0300

    add projects for first lesson

--- BaseTypes/Program.cs
+++ BaseTypes/Program.cs
@@ -6,7 +6,27 @@ namespace BaseTypes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            byte @byte = 255;
+            short @short = 0;
+            int @int = 0;
+            long @long = 0;
+            char @char = 'c';
+            string @string = "Hello, World!";
+            DateTime @datetime = DateTime.Now;
+            DateTimeOffset @datetimeOffset = DateTimeOffset.Now;
+            TimeSpan timeSpan = TimeSpan.Zero;
+            object @object = new object();
+
+            Console.WriteLine(@byte);
+            Console.WriteLine(@short);
+            Console.WriteLine(@int);
+            Console.WriteLine(@long);
+            Console.WriteLine(@char);
+            Console.WriteLine(@string);
+            Console.WriteLine(datetime);
+            Console.WriteLine(datetimeOffset);
+            Console.WriteLine(timeSpan);
+            Console.WriteLine(@object);
         }
     }
 }
---
+++ ConsoleInputOutput/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleInputOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Пожалуйста, введите свое имя или что угодно на свой вкус: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine(userInput);
+        }
+    }
+}
